0d436a7f40d8984f42238f2a59e35d451d72e9bf
wmatecki <wmatecki>
2022-09-22 23:20:08 +0100

    accept cookied

--- Api9Gag/Controllers/V1Controller.cs
+++ Api9Gag/Controllers/V1Controller.cs
@@ -1,5 +1,6 @@
 using Api9Gag.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using PuppeteerSharp;
 
@@ -10,32 +11,47 @@ namespace Api9Gag.Controllers
     public class V1 : ControllerBase
     {
         [HttpGet(Name = "GetPosts")]
-        public async Task<List<Post>> GetPosts(string category, int count)
+        public async Task<List<Post>> GetPosts(int pagesCount)
         {
             var options = new LaunchOptions
             {
                 Headless = true,
-                ExecutablePath = "/usr/bin/chromium",
+                ExecutablePath = "C:\\Users\\wmate\\Downloads\\chrome-win\\chrome-win\\chrome.exe",
+                //ExecutablePath = "/usr/bin/chromium",
                 Args = new[] { "--no-sandbox", "--disable-setuid-sandbox" }
             };
             var posts = new List<Post>();
             using (var browser = await Puppeteer.LaunchAsync(options))
             {
                 int iterationsCount = 0;
-                var url = "https://www.9gag.com";
-                while (posts.Count < count && iterationsCount < 100)
+                var url = "https://9gag.com/";
+                var page = await browser.NewPageAsync();
+                await page.GoToAsync(url);//todo get info
+
+                Thread.Sleep(3000);
+                await page.Keyboard.PressAsync("Tab");
+                Thread.Sleep(1000);
+                await page.Keyboard.PressAsync("Tab");
+                Thread.Sleep(1000);
+                await page.Keyboard.PressAsync("Tab");
+                Thread.Sleep(1000);
+                await page.Keyboard.PressAsync("Tab");
+                Thread.Sleep(1000);
+                await page.Keyboard.PressAsync("Enter");
+
+                while (pagesCount > 0 && iterationsCount < 100)
                 {
-                    var page = await browser.NewPageAsync();
-                    await page.GoToAsync(url);//todo get info
-                    var infoJson = await page.GetContentAsync();
-                    var info = JsonConvert.DeserializeObject<PostsInfo>(infoJson);
-                    posts.AddRange(info.Posts);
-                    url = url + "";//todo
+                    await page.Keyboard.PressAsync("End");
+                    Thread.Sleep(4000);
+                    pagesCount--;
+                    iterationsCount++;
                 }
+                await Task.Delay(60000);
 
-                //page.Keyboard.PressAsync("end");
-                //await page.ScreenshotAsync("C:\\Users\\wmate\\Downloads\\chrome-win\\screenshots\\test2.png");
-
+                var infoJson = await page.GetContentAsync();
+                var info = infoJson.Split("src=\"").Select(x => x.Substring(0, x.IndexOf("\""))).ToList();
+                await page.ScreenshotAsync("C:\\Users\\wmate\\Downloads\\chrome-win\\screenshots\\test2.png");
+                System.IO.File.WriteAllText("C:\\Users\\wmate\\Downloads\\chrome-win\\screenshots\\test2.html", infoJson);
             }
             return posts;
         }
