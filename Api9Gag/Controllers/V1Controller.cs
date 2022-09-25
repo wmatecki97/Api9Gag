9d35a935db322b5f67cb00087276b782758c0997
wmatecki <wmatecki>
2022-09-25 12:52:31 +0200

    no headless but working

--- Api9Gag/Controllers/V1Controller.cs
+++ Api9Gag/Controllers/V1Controller.cs
@@ -15,7 +15,7 @@ namespace Api9Gag.Controllers
         {
             var options = new LaunchOptions
             {
-                Headless = true,
+                Headless = false,
                 ExecutablePath = "C:\\Users\\wmate\\Downloads\\chrome-win\\chrome-win\\chrome.exe",
                 //ExecutablePath = "/usr/bin/chromium",
                 Args = new[] { "--no-sandbox", "--disable-setuid-sandbox" }
@@ -27,29 +27,8 @@ namespace Api9Gag.Controllers
                 var url = "https://9gag.com/";
                 var page = await browser.NewPageAsync();
                 await page.GoToAsync(url);//todo get info
-
-                Thread.Sleep(3000);
-                await page.Keyboard.PressAsync("Tab");
-                Thread.Sleep(1000);
-                await page.Keyboard.PressAsync("Tab");
-                Thread.Sleep(1000);
-                await page.Keyboard.PressAsync("Tab");
-                Thread.Sleep(1000);
-                await page.Keyboard.PressAsync("Tab");
-                Thread.Sleep(1000);
-                await page.Keyboard.PressAsync("Enter");
-
-                while (pagesCount > 0 && iterationsCount < 100)
-                {
-                    await page.Keyboard.PressAsync("End");
-                    Thread.Sleep(4000);
-                    pagesCount--;
-                    iterationsCount++;
-                }
-                await Task.Delay(60000);
-
                 var infoJson = await page.GetContentAsync();
-                var info = infoJson.Split("src=\"").Select(x => x.Substring(0, x.IndexOf("\""))).ToList();
+                //var info = infoJson.Split("src=\"").Select(x => x.Substring(0, x.IndexOf("\""))).ToList();
                 await page.ScreenshotAsync("C:\\Users\\wmate\\Downloads\\chrome-win\\screenshots\\test2.png");
                 System.IO.File.WriteAllText("C:\\Users\\wmate\\Downloads\\chrome-win\\screenshots\\test2.html", infoJson);
             }
